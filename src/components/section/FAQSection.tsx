1a2ec051083e02f76ef4c35e61d954d435df3264
Hamza6-hash <mastank387@gmail.com>
2024-09-29 11:19:12 +0500

    add faq section

---
+++ src/components/section/FAQSection.tsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+
+const faqData = [
+  {
+    question: "What is the purpose of this website?",
+    answer:
+      "This website is designed to provide information about the company and its products.",
+  },
+  {
+    question: "What is the purpose of this website?",
+    answer:
+      "This website is designed to provide information about the company and its products.",
+  },
+];
+
+const FAQSection = () => {
+  const [active, setActive] = useState<number | null>(null);
+  const answerRefs = useRef<(HTMLParagraphElement | null)[]>([]);
+
+  const handleClick = (index: number) => {
+    setActive((prevActive) => (prevActive === index ? null : index));
+  };
+
+  useEffect(() => {
+    answerRefs.current.forEach((ref, index) => {
+      if (ref) {
+        if (active === index) {
+          ref.style.maxHeight = `${ref.scrollHeight}px`;
+        } else {
+          ref.style.maxHeight = "0px";
+        }
+      }
+    });
+  }, [active]);
+
+  return (
+    <div className="md:pt-[64px] md:pb-[128px] pb-[32px] pt-[48px] max-md:px-8">
+      <p className="text-[38px] text-primary max-md:text-[30px]">
+        Frequently asked questions
+      </p>
+      <div className="space-y-8 max-md:mt-12 mt-8">
+        {faqData.map((item, index) => (
+          <div
+            className="flex pb-8 border-b border-[#e6e6e6] dark:border-[#ffffff33] flex-col gap-4"
+            key={index}
+          >
+            <button
+              className="text-[28px] max-md:text-[24px] text-start text-secondary font-medium flex justify-between items-center"
+              onClick={() => handleClick(index)}
+            >
+              <span className="max-md:max-w-[320px]">
+
+              {item.question}
+              </span>
+              {/* <span
+                className={`transform transition-transform duration-300 ${
+                  active === index ? "rotate-180" : ""
+                }`}
+              > */}
+              <svg
+                className={`${
+                  active === index ? "rotate-180" : ""
+                } transform transition-transform duration-300`}
+                width="36px"
+                height="36px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    d="M6 9L12 15L18 9"
+                    stroke="#ededed"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>{" "}
+                </g>
+              </svg>
+              {/* </span> */}
+            </button>
+            <p
+              ref={(el) => {
+                if (el) {
+                  answerRefs.current[index] = el;
+                }
+              }}
+              className="text-[24px] text-secondary leading-[36px] tracking-[-0.5px] overflow-hidden transition-all duration-300 ease-in-out"
+              style={{ maxHeight: "0px" }}
+            >
+              {item.answer}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FAQSection;
--- src/components/section/IntroSection1.tsx
+++ src/components/section/IntroSection1.tsx
@@ -5,7 +5,7 @@ import Button from "../Atoms/Button";
 // bg-gradient-to-r from-gray-800 via-gray-900 to-black
 const IntroSection1 = () => {
   return (
-    <div className="pb-12 pt-16 px-8 max-w-[1800px] mx-auto">
+    <div className="pb-12 pt-6 md:pt-24 md:pb-20 px-8 max-w-[1800px] mx-auto">
       <div className="flex flex-col items-center justify-center gap-8">
         <div className="flex flex-col gap-6 items-center justify-center relative">
           <div className="w-24 h-24 mb-4 rounded-full overflow-hidden relative">
@@ -18,14 +18,14 @@ const IntroSection1 = () => {
           <button className="absolute top-20 w-24 text-[12px] h-[28px] dark:bg-secondary bg-primary rounded-md text-primary dark:text-black font-medium">
             Expert
           </button>
-          <h1 className="text-4xl font-bold dark:text-primary text-quaternary">Hello, I m Hamza</h1>
+          <h1 className="text-4xl font-bold dark:text-primary text-quaternary text-shadow-lg">Hello, I m Hamza</h1>
           <p className="text-lg dark:text-secondary text-quaternary">I m a software engineer</p>
         </div>
-        <div className="flex gap-3 items-center">
-          <Button size="large">
+        <div className="flex gap-3 items-center max-md:flex-col">
+          <Button size="large" className="font-normal">
             Download CV
           </Button>
-          <Button size="large">
+          <Button size="large" className="font-normal">
             Contact me
           </Button>
         </div>
